df7c99513fce28c1bf9870885c9b0af9ee9b750d
MongiLearnsToCode <sandiledlmn@gmail.com>
2025-08-29 23:52:19 +0200

    Regenerate Convex types to fix build error.

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as helloWorld from "../helloWorld.js";
+import type * as profiles from "../profiles.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -25,6 +26,7 @@ import type * as helloWorld from "../helloWorld.js";
  */
 declare const fullApi: ApiFromModules<{
   helloWorld: typeof helloWorld;
+  profiles: typeof profiles;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
